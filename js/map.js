552baab925d86eb74981140ea31482528b8bd73e
Daria <daria.mart21@gmail.com>
2021-03-29 20:45:46 +0300

    Add function generateNotices

---
+++ js/map.js
@@ -0,0 +1,92 @@
+let DATA_NOTICES = {
+  COUNT_USERS: 8,
+  // Диапозон цен
+  MIN_PRICE: 1000,
+  MAX_PRICE: 1000000,
+  // Диапозон комнат
+  MIN_ROOMS: 1,
+  MAX_ROOMS: 5,
+  // Диапозон гостей
+  MIN_GUESTS: 1,
+  MAX_GUESTS: 10,
+  // Координаты 
+  MIN_X: 300,
+  MAX_X: 900,
+  MIN_Y: 130,
+  MAX_Y: 630,
+  TITLES: ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
+  TYPE_OF_ROOMS: ['palace', 'flat', 'house', 'bungalo'],
+  TIME: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+}
+
+// функция, возвращающая случайное число из диапозона
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// функция, возвращающая случайное значение массива 
+function getRandomElement(array) {
+  let randomIndex = Math.floor(Math.random() * array.length)
+  return array[randomIndex]
+}
+
+// функция, возвращающая перемешанный по алгоритму «Тасование Фишера — Йетса» массив
+function shuffleArray(array) {
+  let mixedArray = array.slice()
+  for (let i = mixedArray.length - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    [mixedArray[i], mixedArray[randomIndex]] = [mixedArray[randomIndex], mixedArray[i]];
+  }
+  return mixedArray;
+}
+
+// функция, возвращающая массив случайной длины
+function getRandomArrayLength(array) {
+  return cloneArray = array.slice(0, getRandomNumber(1, array.length))
+}
+
+// Функция, генерирующая массив аватарок
+function generateAvatars() {
+  let avatars = []
+  for (let i = 0; i < DATA_NOTICES.COUNT_USERS; i++) {
+    avatars.push(`img/avatars/user0${i+1}.png`)
+  }
+  return avatars
+}
+
+// Функция, возвращающая массив с объектами, описывающими похожие объявления
+function generateNotices() {
+  let notices = []
+  let noticeAvatars = shuffleArray(generateAvatars())
+  let noticeTitles = shuffleArray(DATA_NOTICES.TITLES)
+  for (let i = 0; i < DATA_NOTICES.COUNT_USERS; i++) {
+    let locationX = getRandomNumber(DATA_NOTICES.MIN_X, DATA_NOTICES.MAX_X)
+    let locationY = getRandomNumber(DATA_NOTICES.MIN_Y, DATA_NOTICES.MAX_Y)
+    notices.push({
+      author: {avatar: noticeAvatars[i]},
+      offer:
+      {
+        title: noticeTitles[i],
+        adress: (locationX + ', ' + locationY),
+        price: getRandomNumber(DATA_NOTICES.MIN_PRICE, DATA_NOTICES.MAX_PRICE),
+        type: getRandomElement(DATA_NOTICES.TYPE_OF_ROOMS),
+        rooms: getRandomNumber(DATA_NOTICES.MIN_ROOMS, DATA_NOTICES.MAX_ROOMS),
+        guests: getRandomNumber(DATA_NOTICES.MIN_GUESTS, DATA_NOTICES.MAX_GUESTS),
+        checkin: getRandomElement(DATA_NOTICES.TIME),
+        checkout: getRandomElement(DATA_NOTICES.TIME),
+        features: getRandomArrayLength(DATA_NOTICES.FEATURES),
+        description: '',
+        photos: shuffleArray(DATA_NOTICES.PHOTOS)
+      },
+      location:
+      {
+        x: locationX,
+        y: locationY
+      }
+    })
+  }
+  return notices;
+}
+console.log(generateNotices())
