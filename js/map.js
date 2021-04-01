2b705b8caa03f31761542cf0094a8d0077a4b607
Daria <daria.mart21@gmail.com>
2021-04-01 12:10:11 +0300

    Add function mapPins for create pins, add function getCard for create notice card. Refactoring
    Replace files: index.html, style.css, all img. Add normalize.css, favicon, fonts

--- js/map.js
+++ js/map.js
@@ -1,4 +1,6 @@
-let DATA_NOTICES = {
+'use strict'
+
+const DATA_NOTICES = {
   COUNT_USERS: 8,
   // Диапозон цен
   MIN_PRICE: 1000,
@@ -21,35 +23,28 @@ let DATA_NOTICES = {
   PHOTOS: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
 }
 
-// функция, возвращающая случайное число из диапозона
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+// Функция, возвращающая случайное число из диапозона
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
-// функция, возвращающая случайное значение массива 
-function getRandomElement(array) {
-  let randomIndex = Math.floor(Math.random() * array.length)
-  return array[randomIndex]
-}
+// Функция, возвращающая случайное значение массива 
+const getRandomElement = (array) => array[Math.floor(Math.random() * array.length)]
 
-// функция, возвращающая перемешанный по алгоритму «Тасование Фишера — Йетса» массив
-function shuffleArray(array) {
+// Функция, возвращающая перемешанный по алгоритму «Тасование Фишера — Йетса» массив
+const shuffleArray = (array) => {
   let mixedArray = array.slice()
   for (let i = mixedArray.length - 1; i > 0; i--) {
-    let randomIndex = Math.floor(Math.random() * (i + 1));
+    const randomIndex = Math.floor(Math.random() * (i + 1));
     [mixedArray[i], mixedArray[randomIndex]] = [mixedArray[randomIndex], mixedArray[i]];
   }
   return mixedArray;
 }
 
-// функция, возвращающая массив случайной длины
-function getRandomArrayLength(array) {
-  return cloneArray = array.slice(0, getRandomNumber(1, array.length))
-}
+// Функция, возвращающая массив случайной длины
+const getRandomArrayLength = (array) => array.slice(0, getRandomNumber(1, array.length))
 
 // Функция, генерирующая массив аватарок
-function generateAvatars() {
-  let avatars = []
+const generateAvatars = () => {
+  const avatars = []
   for (let i = 0; i < DATA_NOTICES.COUNT_USERS; i++) {
     avatars.push(`img/avatars/user0${i+1}.png`)
   }
@@ -57,13 +52,14 @@ function generateAvatars() {
 }
 
 // Функция, возвращающая массив с объектами, описывающими похожие объявления
-function generateNotices() {
-  let notices = []
-  let noticeAvatars = shuffleArray(generateAvatars())
-  let noticeTitles = shuffleArray(DATA_NOTICES.TITLES)
+const generateNotices = () => {
+  const notices = []
+  const noticeAvatars = shuffleArray(generateAvatars())
+  const noticeTitles = shuffleArray(DATA_NOTICES.TITLES)
+  const noticeFeature = shuffleArray(DATA_NOTICES.FEATURES)
   for (let i = 0; i < DATA_NOTICES.COUNT_USERS; i++) {
-    let locationX = getRandomNumber(DATA_NOTICES.MIN_X, DATA_NOTICES.MAX_X)
-    let locationY = getRandomNumber(DATA_NOTICES.MIN_Y, DATA_NOTICES.MAX_Y)
+    const locationX = getRandomNumber(DATA_NOTICES.MIN_X, DATA_NOTICES.MAX_X)
+    const locationY = getRandomNumber(DATA_NOTICES.MIN_Y, DATA_NOTICES.MAX_Y)
     notices.push({
       author: {avatar: noticeAvatars[i]},
       offer:
@@ -76,7 +72,7 @@ function generateNotices() {
         guests: getRandomNumber(DATA_NOTICES.MIN_GUESTS, DATA_NOTICES.MAX_GUESTS),
         checkin: getRandomElement(DATA_NOTICES.TIME),
         checkout: getRandomElement(DATA_NOTICES.TIME),
-        features: getRandomArrayLength(DATA_NOTICES.FEATURES),
+        features: getRandomArrayLength(noticeFeature),
         description: '',
         photos: shuffleArray(DATA_NOTICES.PHOTOS)
       },
@@ -89,4 +85,122 @@ function generateNotices() {
   }
   return notices;
 }
-console.log(generateNotices())
+const getNotices = generateNotices()
+
+// Делаем карту активной (временное решение)
+const pinMap = document.querySelector('.map')
+pinMap.classList.remove('map--faded')
+
+// Делаем метки
+const mapPins = document.querySelector('.map__pins')
+
+const getPins = (data) => {
+  const pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin')
+  const fragment = document.createDocumentFragment()
+  for (let i = 0; i < data.length; i++) {
+    const pin = pinTemplate.cloneNode(true)
+    const pinImg = pin.querySelector('img')
+    pin.style = `left: ${data[i].location.x - pinImg.width / 2}px; top: ${data[i].location.y - pinImg.height / 2}px`
+    pinImg.src = data[i].author.avatar
+    pinImg.alt = data[i].offer.title
+    fragment.append(pin)
+  }
+  return fragment
+}
+mapPins.append(getPins(getNotices))
+
+// Создаем карточку объявления 
+const map = document.querySelector('.map')
+const mapFilters = document.querySelector('.map__filters-container') // Элемент, перед которым будем вставлять карточку
+
+const getCard = (data) => {
+  const cardTemplate = document.querySelector('#card').content.querySelector('.map__card')
+  const card = cardTemplate.cloneNode(true)
+  const {title, adress, price, type, rooms, guests, checkin, checkout, features, description, photos} = data.offer
+  const {avatar} = data.author
+
+  // Плюрализация комнат и гостей
+  const pr = new Intl.PluralRules('ru');
+  const suffixesForGuests = new Map([
+    ['one', 'гостя'],
+    ['many', 'гостей'],
+    ['few', 'гостей'],
+  ])
+  const suffixesForRooms = new Map([
+    ['one', 'комната'],
+    ['many', 'комнат'],
+    ['few', 'комнаты'],
+  ])
+  
+  const formatGuests = (n) => {
+    const rule = pr.select(n);
+    const suffix = suffixesForGuests.get(rule);
+    return `${n} ${suffix}`;
+  };
+  const getGuests = formatGuests(guests)
+  
+  const formatRooms = (n) => {
+    const rule = pr.select(n);
+    const suffix = suffixesForRooms.get(rule);
+    return `${n} ${suffix}`;
+  };
+  const getRooms = formatRooms(rooms)
+  // Вывод удобств жилья
+  const getFeatures =  (array) => {
+    const cardFragment = document.createDocumentFragment()
+    card.querySelector('.popup__features').innerHTML = ''
+    array.forEach(element => {
+      const cardFeature = document.createElement('li')
+      cardFeature.className = `popup__feature popup__feature--${element}`
+      cardFragment.appendChild(cardFeature)
+    });
+    return cardFragment
+  }
+  // Перевод типа жилья на русский язык
+  const cardType = card.querySelector('.popup__type')
+  const Housing = {
+    FLAT: `flat`,
+    BUNGALO: `bungalo`,
+    HOUSE: `house`,
+    PALACE: `palace`,
+  }
+  switch (type) {
+    case Housing.PALACE:
+      cardType.textContent = 'Дворец'
+      break
+    case Housing.FLAT:
+      cardType.textContent = 'Квартира'
+      break
+    case Housing.HOUSE:
+      cardType.textContent = 'Дом'
+      break
+    case Housing.BUNGALO:
+      cardType.textContent = 'Бунгало'
+      break
+  }
+  // Вывод фотографий жилья
+  const getPhotos = (array) => {
+    const cardPhoto = card.querySelector('.popup__photos').querySelector('img')
+    const cardFragment = document.createDocumentFragment()
+    card.querySelector('.popup__photos').innerHTML = ''
+    array.forEach(element => {
+      const newCardPhoto = cardPhoto.cloneNode(true)
+      newCardPhoto.src = element
+      cardFragment.append(newCardPhoto)
+    })
+    return cardFragment
+  }
+  // Собираем карточку
+  card.querySelector('.popup__title').textContent = title
+  card.querySelector('.popup__text--address').textContent = adress // ?
+  card.querySelector('.popup__text--price').textContent = `${price}₽/ночь`
+  card.querySelector('.popup__text--capacity').textContent = `${getRooms} для ${getGuests}`
+  card.querySelector('.popup__text--time').textContent = `Заезд после ${checkin}, выезд до ${checkout}`
+  card.querySelector('.popup__features').append(getFeatures(features))
+  card.querySelector('.popup__description').textContent = description
+  card.querySelector('.popup__photos').append(getPhotos(photos))
+  card.querySelector('.popup__avatar').src = avatar
+  return card
+}
+map.insertBefore(getCard(getNotices[0]), mapFilters)
+
