0a60aa7131fd91bd4417c58341f6d86e750167e4
Daria <daria.mart21@gmail.com>
2021-05-04 16:17:09 +0300

    Add function debounce for filtration

---
+++ js/debounce.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(() => {
+  window.debounce = (func, wait) => {
+    let timeout
+    return function() {
+      const context = this
+      const args = arguments
+  
+      const later = function() {
+        timeout = null
+        func.apply(context, args)
+      }
+      clearTimeout(timeout)
+      timeout = setTimeout(later, wait)
+    }
+  }
+
+})()
